f7f973aad6b290c189bd3c08e6ef3f750145a405
Nobuhiro Ito <iseebi@iseteki.net>
2017-09-29 18:50:02 +0900

    add GPLv3 declaration

--- Classes/EBIiOSDevice.h
+++ Classes/EBIiOSDevice.h
@@ -1,10 +1,17 @@
+// Copyright © 2017 Nobuhiro Ito
+// This file is part of EBIiOSDeviceWatcher.
 //
-//  EBIMobileDevice.h
-//  EBIMobileDeviceWatcher
+// EBIiOSDeviceWatcher is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License any later version.
 //
-//  Created by Nobuhiro Ito on 2017/01/06.
-//  Copyright © 2017 Nobuhiro Ito. All rights reserved.
+// EBIiOSDeviceWatcher is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
+// You should have received a copy of the GNU General Public License
+// along with EBIiOSDeviceWatcher.  If not, see <http://www.gnu.org/licenses/>.
 
 #import <Foundation/Foundation.h>
 
